8f2ee9ccfd624077a12acadd1839d13a7d94fb8a
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-02-03 17:19:53 +0100

    update

---
+++ exercise/src/components/Counter.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+
+class Counter extends Component {
+    state = {
+        count: this.props.initialValue,
+    }
+    constructor(props) {
+        super(props);
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + this.props.incrementBy
+                }
+            })
+        }, 1000);
+
+    }
+    render() {
+        return <h1>{this.state.count}</h1>
+    };
+    
+}
+
+Counter.defaultProps = {
+    initialValue: 0,
+    incrementBy: 1,
+}
+
+export default Counter;
