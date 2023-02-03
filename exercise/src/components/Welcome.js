5b0eea8e73f2ba853212b4dae844c88976028a58
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-02-03 16:35:31 +0100

    done ex18

--- exercise/src/components/Welcome.js
+++ exercise/src/components/Welcome.js
@@ -2,8 +2,8 @@ import Age from "./Age";
 
 function Welcome({name = 'World', age}) {
     return <>
-    <p>Hello, {name}!</p>
-    {(age>18 && age<65) && <Age age={age} />}
+    <p>Hello, {<strong>{name}</strong>}!</p>
+    {(age>18 && age<65 && name=='John') && <Age age={age} />}
     </>
 }
 
