3356a6d1e3f21e5804eccd00c82af16458f03f0f
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-02-05 22:07:58 +0100

    done

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -2,8 +2,8 @@ import Container from "./components/Container";
 
 function App() {
     return <>
-        <Container>
-            <h1>Hello World!</h1>
+        <Container title='Hello World!'>
+            <p>This is a white box with a red border</p>
         </Container>
     </>
 }
--- exercise/src/components/Container.js
+++ exercise/src/components/Container.js
@@ -1,6 +1,7 @@
 function Container(props) {
-    return <div className='bg-white border-8 border-red-700 blue-title'>
-    {props.children}
+    return <div className='bg-white border-8 border-red-700 p-2'>
+        <h1 className="blue-title">{props.title}</h1>
+        {props.children}
     </div>
 }
 
