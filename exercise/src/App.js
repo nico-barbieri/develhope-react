205381b72271d890ee7cd52a76bb039a108ab987
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-02-06 01:48:23 +0100

    done

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,8 +1,30 @@
-import Component from "./components/Component";
+import { useState } from "react";
+import DisplayLanguage from "./components/DisplayLanguage";
+import { LanguageContext } from "./components/LanguageContext";
 
 function App() {
+    const [state, setState] = useState({
+        language: 'en',
+    })
+
+    const handleLanguageChange = (e) => {
+        const language = e.target.value;
+        setState(state => {
+            return {
+                ...state,
+                language: language,
+            }
+        })
+    }
+
     return <>
-        <Component />
+        <select value={state.language} onChange={handleLanguageChange}>
+            <option value={'en'}>English</option>
+            <option value={'it'}>Italiano</option>
+        </select>
+        <LanguageContext.Provider value={state.language}>
+            <DisplayLanguage />
+        </LanguageContext.Provider>
     </>
 }
 
